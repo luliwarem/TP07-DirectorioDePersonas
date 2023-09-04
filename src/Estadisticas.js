47af1b4733d454b2011278e9a64d7ae8aa972830
LUCIANA WAREMKRAUT <47026652@ort>
2023-09-04 15:33:44 -0300

    TERMINAMOS DALE LOCOOOOO

--- src/Estadisticas.js
+++ src/Estadisticas.js
@@ -1,29 +1,42 @@
+import { useState, useEffect } from "react";
 import personitas from "./Personas.json";
+import "./Estadisticas.css"
 
 const Estadisticas = () => {
-    
     const result = personitas.filter((p) => parseInt(p.edad) > 35);
 
-    const mayorEdad = personitas.reduce((max , p) => Math.max( max, parseInt(p.edad)), -Infinity)
-    const menorEdad = personitas.reduce((min , p) => Math.min( min, parseInt(p.edad)), Infinity)
-    console.log(mayorEdad, " ",menorEdad)
-    const personasMayorEdad = personitas.filter((p) => parseInt(p.edad) === mayorEdad)
-    const personasMenorEdad = personitas.filter((p) => parseInt(p.edad) === menorEdad)
+    const mayorEdad = personitas.reduce((prev, current) => {
+        return parseInt(current.edad) > parseInt(prev.edad) ? current : prev
+    })
 
+    const menorEdad = personitas.reduce((prev, current) => {
+        return parseInt(current.edad) < parseInt(prev.edad) ? current : prev
+    })
 
+    const result2 = personitas.filter((p) => parseInt(mayorEdad.edad) === parseInt(p.edad))
+    const result3 = personitas.filter((p) => parseInt(menorEdad.edad) === parseInt(p.edad))
+
+    console.log(result3)
     return (
-            <div><p>
-                mayores de 35: {result.length}
-                <br></br>
-
-                las personas de mayor edad son: {personasMayorEdad}
-
-                <br></br>
-
-                las personas de menor edad son: {personasMenorEdad}
-
-            </p>
-            </div >
+        <div className="centrar">
+        <div class="parent">
+          <div class="card">
+              <div class="logo">
+                  <span class="circle circle1"></span>
+                  <span class="circle circle2"></span>
+                  <span class="circle circle3"></span>
+                  <span class="circle circle4"></span>
+                  <span class="circle circle5"></span>
+              </div>
+              <div class="glass"></div>
+              <div class="content">
+                  <span class="title">Hay {result.length} personas mayores de 35</span>
+                  <span class="text">Las personas de mayor edad son: {result2.map((p) => p.nombre)} </span>
+                  <span class="text">Las personas de menor edad son: {result3.map((p) => p.nombre)}</span>
+              </div>
+          </div>
+      </div>
+     </div>
     )
 
 }
